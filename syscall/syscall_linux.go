5e9619054890408dcd49f6c8df91c4d97d6f898c
lysShub <lysShub@gmail.com>
2024-07-07 04:54:52 +0800

    add SetBPF

--- syscall/syscall_linux.go
+++ syscall/syscall_linux.go
@@ -6,11 +6,68 @@ package syscall
 import (
 	"net"
 	"net/netip"
+	"syscall"
 	"unsafe"
 
 	"github.com/pkg/errors"
+	"golang.org/x/net/bpf"
 	"golang.org/x/sys/unix"
 )
+
+func SetRawConnBPF(
+	conn interface {
+		SyscallConn() (syscall.RawConn, error)
+	},
+	ins []bpf.Instruction,
+) error {
+	raw, err := conn.SyscallConn()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	var e error
+	if err := raw.Control(func(fd uintptr) {
+		e = SetBPF(fd, ins)
+	}); err != nil {
+		return errors.WithStack(err)
+	}
+	return e
+}
+
+func SetBPF(fd uintptr, ins []bpf.Instruction) error {
+	// drain buffered packet
+	// https://natanyellin.com/posts/ebpf-filtering-done-right/
+	err := setBPF(fd, []bpf.Instruction{bpf.RetConstant{Val: 0}})
+	if err != nil {
+		return err
+	}
+	var b = make([]byte, 1)
+	for {
+		n, _, _ := unix.Recvfrom(int(fd), b, unix.MSG_DONTWAIT)
+		if n < 0 {
+			break
+		}
+	}
+
+	return setBPF(fd, ins)
+}
+
+func setBPF(fd uintptr, ins []bpf.Instruction) error {
+	var prog *unix.SockFprog
+	if rawIns, err := bpf.Assemble(ins); err != nil {
+		return err
+	} else {
+		prog = &unix.SockFprog{
+			Len:    uint16(len(rawIns)),
+			Filter: (*unix.SockFilter)(unsafe.Pointer(&rawIns[0])),
+		}
+	}
+
+	err := unix.SetsockoptSockFprog(
+		int(fd), unix.SOL_SOCKET, unix.SO_ATTACH_FILTER, prog,
+	)
+	return err
+}
 
 func IoctlGifname(ifi int) (string, error) {
 	fd, err := unix.Socket(unix.AF_PACKET, unix.SOCK_DGRAM, 0)
